ca4630b21b54fb16e316ee7bbabff9942fc3841b
Alexandra-Jev <32392524+Alexandra-Jev@users.noreply.github.com>
2017-10-10 23:08:38 +0200

    Add files via upload

---
+++ learn_ruby-master/01_temperature/temperature.rb
@@ -0,0 +1,13 @@
+#write your code here
++def ftoc(num)
+ +  return (num - 32) / 9 * 5
+ +end
+ +
+ +# puts ftoc(32)
+ +
+ +def ctof(num)
+ +  return (num * 9.0 / 5.0) + 32.0
+ +end
+ +
+ +# puts ctof(37)
+
